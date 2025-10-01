55ad66c0098de7a13316edc8985a5c8d725105c6
Gustavo Feitosa <gustavo.prog782@gmail.com>
2025-10-01 16:50:23 -0300

    Add files via upload
    Meu SQL sobre músicas

--- selects_musicas.sql
+++ selects_musicas.sql
@@ -1,5 +1,5 @@
 -- Mostrando a tabela
-SELECT * from musicas
+SELECT * FROM musicas
 
 -- 1. Músicas mais populares (mais streams)
 SELECT titulo, artista, streaming_count 
@@ -48,23 +48,36 @@ LIMIT 10;
 -- 7. Quero saber quais os albuns do Michael Jackson estão na lista e quais os anos de lançamento dos albuns
 
 -- 8. Quero ver somente as musicas de Rock da playlist
+SELECT titulo, genero FROM musicas WHERE genero = "Rock"
 
 -- 9. quero ver somente as musicas lançadas nos anos 90
+SELECT titulo, ano_lancamento FROM musicas WHERE ano_lancamento >=1090 AND ano_lancamento <= 1999
 
 -- 10. quero ver musicas lançadas apartir dos anos 2000
+SELECT titulo, ano_lancamento FROM musicas WHERE ano_lancamento >=2000
 
 -- 11. Adicione as informações a respeito da sua musica favorita.
+INSERT INTO musicas (titulo, artista, album, genero, duracao_segundos, ano_lancamento, streaming_count) 
+VALUES ('Carnaval', 'Kamaitachi', 'Festa', 'Pop', 195 , 2023, 3100000)
 
 -- 12 .Adicione qualquer música de Funk que o professor odeia
+INSERT INTO musicas (titulo, artista, album, genero, duracao_segundos, ano_lancamento, streaming_count) 
+VALUES ('Voz do Coração', 'MC Cabelinho', 'Minha Raiz', 'Funk', 236, 2018, 540000000)
 
 -- 13. remova essa música que você acabou de adicionar.
+DELETE FROM musicas WHERE id = 125
 
 -- 14. Conte quantas músicas a banda AC/DC tem 
+SELECT titulo, artista,count(*) as total_musicas FROM musicas WHERE artista = "AC/DC"
+SELECT count(*) as total_musicas FROM musicas WHERE artista = "AC/DC"
 
 -- 15. conte quantas músicas de POP tem na playlist 
+SELECT count (*) FROM musicas WHERE genero = "Pop"
 
 -- 16. Ordene as musícas da mais velha para a mais nova.
+SELECT * FROM musicas ORDER by ano_lancamento asc
 
 
+SELECT * FROM musicas ORDER by id DESC
 
 
